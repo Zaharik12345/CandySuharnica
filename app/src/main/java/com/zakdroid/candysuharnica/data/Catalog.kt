a7c9059dbfe8cf2b5c86c3ee60bdd6facc9c2b02
iliar <Ilia.rodkin@gmail.com>
2021-12-03 13:20:35 +0300

    add catalog fragment#1

---
+++ app/src/main/java/com/zakdroid/candysuharnica/data/Catalog.kt
@@ -0,0 +1,4 @@
+package com.zakdroid.candysuharnica.data
+
+class Catalog {
+}
--- app/src/main/java/com/zakdroid/candysuharnica/ui/MainActivity.kt
+++ app/src/main/java/com/zakdroid/candysuharnica/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.zakdroid.candysuharnica
+package com.zakdroid.candysuharnica.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zakdroid.candysuharnica.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.catalog_fragment)
     }
 }
