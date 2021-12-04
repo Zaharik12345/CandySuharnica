4cad8261d2330474c83952789d8b2940dab7a5fc
Zaharik12345 <zakhar.sinkevich@mail.ru>
2021-12-04 13:07:32 +0300

    Fix findviewbyid to binding in SplashScreen.kt

--- app/src/main/java/com/zakdroid/candysuharnica/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/zakdroid/candysuharnica/ui/splash/SplashScreen.kt
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import com.zakdroid.candysuharnica.R
+import com.zakdroid.candysuharnica.databinding.ActivitySplashScreenBinding
 import com.zakdroid.candysuharnica.ui.main.MainActivity
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val ivNote = findViewById<ImageView>(R.id.imgViewLogo)
-        ivNote.alpha = 0f
-        ivNote.animate().setDuration(2000).alpha(1f).withEndAction{
+        binding.imgViewLogo.alpha = 0f
+        binding.imgViewLogo.animate().setDuration(2000).alpha(1f).withEndAction{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
