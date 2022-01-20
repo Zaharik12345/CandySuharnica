deb696230356db616898cf0d82ba5c950aa56d78
Zaharik12345 <zakhar.sinkevich@mail.ru>
2022-01-20 18:44:03 +0300

    #m.b

--- app/src/main/java/com/zakdroid/candysuharnica/data/model/catalog/CatalogItem.kt
+++ app/src/main/java/com/zakdroid/candysuharnica/data/model/catalog/CatalogItem.kt
@@ -3,11 +3,14 @@ package com.zakdroid.candysuharnica.data.model.catalog
 data class CatalogItem(
     val id: String? = "",
     val name: String? = "",
-    val type_id: String? = "", //for filter in the groups
+    val type: String? = "", //for filter in the groups
     val weight: String? = "",
     val imgUrl: String? = "",
     val price: String? = "",
     val priceSale: String? = "",
     var likes: String? = "",
+    var about: String? = "",
+    var product_composition: String? = "",
+    var nutritional_value: String? = "",
     var isLiked: Boolean = false
 )
--- app/src/main/java/com/zakdroid/candysuharnica/data/model/catalog/room/entities/catalogItemDbEntity.kt
+++ app/src/main/java/com/zakdroid/candysuharnica/data/model/catalog/room/entities/catalogItemDbEntity.kt
@@ -8,27 +8,30 @@ import com.zakdroid.candysuharnica.data.model.catalog.CatalogItem
 @Entity(tableName = "catalog")
 
 data class catalogItemDbEntity(
-    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: String,
     @ColumnInfo(name = "imgUrl") val imgUrl: String,
-    @ColumnInfo(name = "likes") val likes: Int,
+    @ColumnInfo(name = "likes") val likes: String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "price") val price: String,
     @ColumnInfo(name = "priceSale") val priceSale: String,
-    @ColumnInfo(name = "weight") val weight: Int,
+    @ColumnInfo(name = "weight") val weight: String,
     @ColumnInfo(name = "about") val about: String,
     @ColumnInfo(name = "product_composition") val product_composition: String,
     @ColumnInfo(name = "nutritional_value") val nutritional_value: String,
-    @ColumnInfo(name = "type_id") val type_id: Int
+    @ColumnInfo(name = "type") val type: String
 ) {
 
     fun toCatalogItem():CatalogItem = CatalogItem(
-        id = id.toString(),
+        id = id,
         name = name,
-        type_id = type_id.toString(),
-        weight = weight.toString(),
+        type = type,
+        weight = weight,
         imgUrl = imgUrl,
         price = price,
         priceSale = priceSale,
-        likes = likes.toString()
+        likes = likes,
+        about = about,
+        product_composition = product_composition,
+        nutritional_value = nutritional_value
     )
 }
